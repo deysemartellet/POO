b95f6f92af730ab79dea6a423d865847b3ed225d
Deyse Martellet <dmartellet@gmail.com>
2024-07-28 20:51:09 -0300

    280724

---
+++ SemAtribuir.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SemAtribuir {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+        String palavra = sc.next();
+        System.out.println(numero + " " + palavra);
+    }
+    
+}
---
+++ Variaveis.java
@@ -0,0 +1,7 @@
+public class Variaveis {
+    public static void main(String[] args) {
+        int numero = 10;
+        String palavra = "Texto";
+        System.out.println("Ola, mundo " + numero + " " + palavra);
+    }
+}
